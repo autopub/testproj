00d55b915564c57273898e7db08e31bdabe74655
BotPub <botpub@autopub.rocks>
2019-07-07 09:41:18 +0200

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+CHANGELOG
+=========
+
+0.1.0 - 2019-07-05
+------------------
+
+Initial project release.
---
+++ tests/test_testproj.py
@@ -0,0 +1,6 @@
+from testproj.hello import hello
+
+
+def test_hello():
+    hello_world = hello()
+    assert hello_world == "Hello World"
